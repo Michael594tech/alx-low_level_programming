b5b5627334bc43acfd917c86eb44c5f07090602d
Michael Tonga <michaeltonga30@gmail.com>
2023-07-18 11:17:15 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,6 @@
  */
 
 void print_times_table(int n)
-
 {
 	int i, j, k;
 
@@ -13,9 +12,9 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for(j = 0; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				k = j*i;
+				k = j * i;
 				if (j == 0)
 				{
 					_putchar(k + '0');
