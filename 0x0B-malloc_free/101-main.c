ffb9458e96548583c4b4a72611db9fa09158b392
Michael594tech <michaeltonga30@gmail.com>
2023-08-24 07:02:09 -0700

    updated

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -1,38 +1,71 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * print_tab - Prints an array of string
- * @tab: The array to print
+ * split_words - Helper function to split a string into words
+ * @str: The input string
  *
- * Return: nothing
+ * Return: An array of pointers to strings (words)
  */
-void print_tab(char **tab)
+char **split_words(char *str)
 {
-	int i;
+    // Count the number of words in the input string
+    int count = 0;
+    char *temp = str;
+    while (*temp)
+    {
+        while (*temp && *temp == ' ')
+            temp++;
+        if (*temp)
+            count++;
+        while (*temp && *temp != ' ')
+            temp++;
+    }
 
-	for (i = 0; tab[i] != NULL; ++i)
-	{
-		printf("%s\n", tab[i]);
-	}
+    // Allocate memory for an array of string pointers
+    char **words = (char **)malloc((count + 1) * sizeof(char *));
+    if (words == NULL)
+    {
+        perror("Memory allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize the array of string pointers
+    int i;
+    for (i = 0; i <= count; i++)
+    {
+        words[i] = NULL;
+    }
+
+    // Extract words from the input string
+    char *word = strtok(str, " ");
+    i = 0;
+    while (word != NULL)
+    {
+        words[i] = strdup(word);
+        if (words[i] == NULL)
+        {
+            perror("Memory allocation error");
+            exit(EXIT_FAILURE);
+        }
+        word = strtok(NULL, " ");
+        i++;
+    }
+
+    return words;
 }
 
 /**
- * main - check the code for ALX School students.
+ * strtow - Splits a string into words
+ * @str: The input string
  *
- * Return: 1 if an error occurred, 0 otherwise
+ * Return: An array of pointers to strings (words)
  */
-int main(void)
+char **strtow(char *str)
 {
-	char **tab;
+    if (str == NULL || *str == '\0')
+        return NULL;
 
-	tab == strtow("		ALX School		#cisfun		");
-	if (tab == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	print_tab(tab);
-	return (0);
+    return split_words(str);
 }
